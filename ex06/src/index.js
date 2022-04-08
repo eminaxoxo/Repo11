0f4eb46104c14bae856ac16c077a1acf6b92b972
eminaxoxo <100778616+eminaxoxo@users.noreply.github.com>
2022-04-08 08:36:23 +0200

    JS games

---
+++ ex06/src/index.js
@@ -0,0 +1,7 @@
+myNestedArray = [["Toblerone", 5]];
+function myNestedFunction(arr) {
+  myNestedArray.push(arr);
+  return myNestedArray;
+}
+console.log(myNestedFunction(["Milka", 3]));
+module.exports = myNestedFunction;
